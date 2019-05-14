2b50d7e57e55de67fd94480f4b97344ebb1855de
Jason Sackey <planetearthjason@gmail.com>
2019-05-14 16:44:31 +0100

    Modal windows for portfolio.

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-console.log('js!');
+var modal = new tingle.modal({
+    footer: true,
+    stickyFooter: false,
+    closeMethods: ['overlay', 'button', 'escape'],
+    closeLabel: "Close",
+    cssClass: ['work-modal'],
+});
+// modal.setFooterContent(content)('modal');
 
+document.querySelectorAll('.work').forEach(element => {
+    // Wrap links
+    var wrapper = document.createElement('a');
+    var index = element.dataset.index;
+    var template = document.querySelector('template[data-index="' + index + '"]').innerHTML;
+    var titleTemplate = element.children[0].innerHTML;
+
+    wrapper.setAttribute('href', '#!');
+    element.parentNode.insertBefore(wrapper, element);
+    wrapper.appendChild(element);
+
+    wrapper.addEventListener('click', () => {
+
+        modal.setContent(titleTemplate + template);
+        modal.open.call(modal);
+
+    });
+});
